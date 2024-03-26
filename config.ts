b6479826432e2583b0aeeac0f5f4d2c8083e9a12
Carlos Eduardo <carlosedlima4@gmail.com>
2024-03-26 18:46:02 -0300

    create gitignore and config.ts file

---
+++ config.ts
@@ -0,0 +1,31 @@
+const dotenv = require('dotenv');
+const { DataSource } = require("typeorm");
+dotenv.config();
+
+const portString = process.env.DB_PORT;
+const userName = process.env.DB_USERNAME
+const password = process.env.DB_PASSWORD
+const database = process.env.DB_NAME
+const port = portString ? parseInt(portString) : undefined;
+
+const PostgresDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: port,
+  username: userName,
+  password: password,
+  database: database,
+  synchronize: true,
+  logging: true,
+  migrations: ["./src/migrations/*.ts"],
+});
+
+PostgresDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
+
+module.exports = PostgresDataSource;
