88ccb60453f240be9223fddcbc7229d63a170f9c
Carlos Eduardo <carlosedlima4@gmail.com>
2024-03-27 18:41:57 -0300

    add swagger.json

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import bodyParser from "body-parser";
-import cors from "cors";
 import dotenv from 'dotenv';
 import express from "express";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 import activityRoutes from "./routes/activityRouter.ts";
 import courseRoutes from "./routes/courseRouter.ts";
 import studantsRoutes from "./routes/studantsRouter.ts";
@@ -12,24 +12,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json()); 
 
+app.use("/estudante", studantsRoutes);
+app.use("/curso", courseRoutes);
+app.use("/tarefa", taskRoutes);
+app.use("/atividade", activityRoutes);
 
-(async () => {
-  app.use("/estudante", studantsRoutes);
-  app.use("/curso", courseRoutes);
-  app.use("/tarefa", taskRoutes);
-  app.use("/atividade", activityRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-
-  try {
-    app.listen(PORT, () => {
-      console.log(`Servidor iniciado na porta ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Erro ao iniciar o servidor:", error);
-  }
-})();
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado na porta ${PORT}`);
+});
 
 export default app;
